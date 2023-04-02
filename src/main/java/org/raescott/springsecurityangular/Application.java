6bb38aad7e962b41af6e58f5fa6adcbc65ea6c25
Richard Scott Smith <pilbender@gmail.com>
2023-04-02 15:29:40 -0700

    Added a method, a rest service for returning Hello World.

--- src/main/java/org/raescott/springsecurityangular/Application.java
+++ src/main/java/org/raescott/springsecurityangular/Application.java
@@ -1,14 +1,23 @@
 package org.raescott.springsecurityangular;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Richard Scott Smith <pilbender@gmail.com>
  */
 @SpringBootApplication
+@RestController
 public class Application {
+  @RequestMapping("resource")
+  public String home() {
+    return "Hello World";
+  }
+
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+    SpringApplication.run(Application.class, args);
 	}
 }
