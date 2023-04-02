3c0fdbc9a371aa455d6f8cfbf4ad8bc12c3ee6cf
Richard Scott Smith <pilbender@gmail.com>
2023-04-02 13:48:26 -0700

    Initial setup of a Spring Boot with Angular integration project.  This builds and runs but doesn't really do anything except print "Hello World!"  This has Spring Security dependencies and core Angular dependencies.

---
+++ src/main/java/org/raescott/springsecurityangular/Application.java
@@ -0,0 +1,14 @@
+package org.raescott.springsecurityangular;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Richard Scott Smith <pilbender@gmail.com>
+ */
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
