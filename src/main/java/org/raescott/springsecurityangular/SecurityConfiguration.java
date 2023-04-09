443f5f6e02cf2b656817fe69b93679ec84a99cfe
Richard Scott Smith <pilbender@gmail.com>
2023-04-09 16:39:12 -0700

    ***Bingo***
    So this appears to do everything we care about.  It's a combination of Form
    and Basic Spring Security.  The form based is working for certain pages
    (/home in this case) and Basic is working in a stateless way as expected where
    you can send the credentials in the URL: http://user:password@localhost:8080/basic/resource.
    So what this means is a different page from the browser will not let the /home
    resource be accessed without authentication but the basic resource with creds
    is available all day long.  This is super good.

--- src/main/java/org/raescott/springsecurityangular/SecurityConfiguration.java
+++ src/main/java/org/raescott/springsecurityangular/SecurityConfiguration.java
@@ -3,15 +3,10 @@ package org.raescott.springsecurityangular;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -36,13 +31,11 @@ public class SecurityConfiguration {
 		return http.build();
 	}
 
-	//@Bean // comment this line to not have this configuration wired in
-	// @Order(1) // No order defaults to last
+	@Bean // comment this line to not have this configuration wired in
+	@Order(2) // No order defaults to last
 	public SecurityFilterChain formFilterChain(HttpSecurity http) throws Exception {
 		http.securityMatcher("/**")
-			.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
-				authorizationManagerRequestMatcherRegistry
-					.requestMatchers("**").permitAll()
+			.authorizeRequests()
 					.requestMatchers(
 			   "/index.html",
 						"/",
@@ -59,8 +52,10 @@ public class SecurityConfiguration {
 						"/**/*.woff2",
 						"/**/*.ttf"
 					).permitAll()
-					.requestMatchers("/**").authenticated();
-			})
+					.requestMatchers("/**").authenticated()
+			.and()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+			.and()
 			.formLogin(form -> form
 				.loginPage("/login")
 				.permitAll())
